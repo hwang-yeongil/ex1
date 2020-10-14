26f3d727a05da5b0f8cd36901ac3ec7d6adf0aaa
jyhwangy4k <jyhwangy4k@gmail.com>
2020-10-14 10:09:31 +0000

    add Card

--- ex1.java
+++ ex1.java
@@ -15,18 +15,48 @@ class ex1{
 
 
   public static void main(String[] args){
-    String number[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
-    // String shape[] = {"Spade","Diamond","Hearth","Clover"};
+    // String number[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    // // String shape[] = {"Spade","Diamond","Hearth","Clover"};
 
-    for (int i = 0; i < number.length; i++) {
-            String shape1 = number[i];
-            System.out.println(shape1 );
-        }
+    // for (int i = 0; i < number.length; i++) {
+    //         String shape1 = number[i];
+    //         System.out.println(shape1 );
+    //     }\
+
+    Card card = new Card();
+
+
+
+    String[][] Cards = new String[4][13];
+    String[] numbers = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    String[] model = {"♡","◇","♣","♠"};
+
+    Cards[0][0] = Card.card("a","b");
+    System.out.println(Cards[0][0]);
+
+    for(int i=0; i<4 ; i++ ){
+     
+      for(int j=0; j<13; j++ ){
+
+        
+        // Cards[i][j] = Card.card(numbers[j],model[i]);
+        // System.out.println(Cards[i][j]);
+      }
+    }
   }
 }
+public class Card{
+  String number;
+  String model;
 
-class card{
-  
+  // void Card(){};
+
+  // void setCard
+
+  void Card(String number, String model){
+    this.number = number;
+    this.model = model;
+  }
 
 
 }
